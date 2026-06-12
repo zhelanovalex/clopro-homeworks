94322301d9ac51a69d5362e2fb813dde80d619a7
Zhelanovalex <zhelanov@gmail.com>
2026-06-12 10:41:12 +0300

    change cloud1_r

--- 15.1/src/providers.tf
+++ 15.1/src/providers.tf
@@ -14,11 +14,3 @@ provider "yandex" {
   zone                     = var.default_zone
   service_account_key_file = file("/root/.config/yandex-cloud/authorized_key.json")
 }
-provider "yandex" {
-  # token                    = var.token
-  alias                    = "zone_b" 
-  cloud_id                 = var.cloud_id
-  folder_id                = var.folder_id
-  zone                     = var.vm_zones_b
-  service_account_key_file = file("/root/.config/yandex-cloud/authorized_key.json")
-}
